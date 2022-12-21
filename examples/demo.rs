ded4131cfd1a706713e18f9853d09981a64bf761
William Woodruff <william@yossarian.net>
2022-12-21 18:08:56 -0500

    demo: allow .der suffixes as well
    Signed-off-by: William Woodruff <william@yossarian.net>

--- examples/demo.rs
+++ examples/demo.rs
@@ -8,7 +8,7 @@ fn main() {
     let path = env::args().nth(1).expect("usage: demo <file>");
     let file = std::fs::File::open(&path).expect("error: couldn't open file");
 
-    let ctl = if path.ends_with(".stl") {
+    let ctl = if path.ends_with(".stl") || path.ends_with(".der") {
         Ctl::from_der(file).expect("failed to load CTL")
     } else if path.ends_with(".cab") {
         Ctl::from_cab(file).expect("failed to load CTL")
